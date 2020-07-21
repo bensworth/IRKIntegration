d6aa91a19b11c053c2f5e5ede61c0423e433f4af
Will Pazner <will.e.p@gmail.com>
2020-07-20 17:02:04 -0700

    Use new nonlinear solvers

--- code/dg_ns/dg_ns.cpp
+++ code/dg_ns/dg_ns.cpp
@@ -3,22 +3,29 @@
 #include "3dg_wrapper.hpp"
 #include "general/binaryio.hpp"
 
+#include <memory>
+
 struct DGIRKOperator : IRKOperator
 {
+   // using PrecKey = std::tuple<int,double,double>;
+   using PrecKey = int;
+
    DGWrapper &dg;
    DGMatrix M;
-   mutable DGMatrix J;
-   std::map<std::pair<double,double>,DGPreconditioner*> prec;
+   std::map<int,DGPreconditioner*> prec;
+   std::vector<DGMatrix *> Js;
    DGPreconditioner *current_prec;
    mutable bool jacobian_updated;
+   Vector z;
 
-   DGIRKOperator(DGWrapper &dg_)
-      : IRKOperator(MPI_COMM_WORLD, dg_.Size(), 0.0, IMPLICIT),
+   DGIRKOperator(DGWrapper &dg_, IRKOperator::ExplicitGradients grad_type)
+      : IRKOperator(MPI_COMM_WORLD, dg_.Size(), 0.0, IMPLICIT, grad_type),
         dg(dg_),
         current_prec(nullptr),
         jacobian_updated(true)
    {
       dg.MassMatrix(M);
+      z.SetSize(dg.Size());
    }
 
    /// Apply action of M*du/dt, y <- L(x,y)
@@ -27,13 +34,45 @@ struct DGIRKOperator : IRKOperator
       dg.Assemble(x, y);
    }
 
-   /// Gradient of L(u, t) w.r.t u evaluated at x
-   virtual Operator &GetExplicitGradient(const Vector &x) const override
+   virtual void SetExplicitGradient(const Vector &u,
+                                    double dt,
+                                    const BlockVector &x,
+                                    const Vector &c) override
    {
-      dg.AssembleJacobian(x, J);
+      for (DGMatrix *J : Js) { delete J; }
+      Js.resize(1);
+      Js[0] = new DGMatrix;
+      add(u, dt, x.GetBlock(c.Size()-1), z);
+      dg.AssembleJacobian(z, *Js[0]);
       jacobian_updated = true;
-      return J;
    }
+
+   virtual void SetExplicitGradients(const Vector &u,
+                                     double dt,
+                                     const BlockVector &x,
+                                     const Vector &c) override
+   {
+      int s = c.Size();
+      for (DGMatrix *J : Js) { delete J; }
+      Js.resize(s);
+      for (int i=0; i<s; ++i)
+      {
+         Js[i] = new DGMatrix;
+         add(u, dt, x.GetBlock(i), z);
+         dg.AssembleJacobian(z, *Js[i]);
+      }
+      jacobian_updated = true;
+   }
+
+    virtual void ExplicitGradientMult(const Vector &x, Vector &y) const override
+    {
+      ExplicitGradientMult(0, x, y);
+    }
+
+    virtual void ExplicitGradientMult(int i, const Vector &x, Vector &y) const override
+    {
+       Js[i]->Mult(x, y);
+    }
 
    /** Apply action mass matrix, y = M*x.
        If not re-implemented, this method simply generates an error. */
@@ -49,13 +88,18 @@ struct DGIRKOperator : IRKOperator
       current_prec->Mult(x, y);
    }
 
+   virtual void ImplicitPrec(int index, const Vector &x, Vector &y) const override
+   {
+      prec.at(index)->Mult(x, y);
+   }
+
    /** Ensures that this->ImplicitPrec() preconditions (\gamma*M - dt*L')
            + index: index of system to solve, [0,s_eff)
            + dt:    time step size
            + type:  eigenvalue type, 1 = real, 2 = complex pair
        These additional parameters are to provide ways to track when
        (\gamma*M - dt*L') must be reconstructed or not to minimize setup. */
-   virtual void SetSystem(int index, double dt, double gamma, int type) override
+   virtual void SetPreconditioner(int index, double dt, double gamma, int type) override
    {
       if (jacobian_updated)
       {
@@ -67,14 +111,52 @@ struct DGIRKOperator : IRKOperator
          prec.clear();
       }
 
-      std::pair<double,double> key = std::make_pair(dt, gamma);
+      PrecKey key = index;
       bool key_exists = prec.find(key) != prec.end();
       if (!key_exists)
       {
-         prec[key] = new DGPreconditioner(gamma, M, dt, J, dg.BlockSize());
+         prec[key] = new DGPreconditioner(gamma, M, dt, *Js[0], dg.BlockSize());
+      }
+      current_prec = prec[key];
+   }
+
+   virtual void SetPreconditioner(int index, double dt, double gamma, Vector weights) override
+   {
+      if (jacobian_updated)
+      {
+         jacobian_updated = false;
+         for (auto &p : prec)
+         {
+            delete p.second;
+         }
+         prec.clear();
       }
 
+      PrecKey key = index;
+      bool key_exists = prec.find(key) != prec.end();
+      if (!key_exists)
+      {
+         DGMatrix B;
+         B.A = Js[0]->A;
+         B.A *= weights[0];
+         for (int i=1; i<weights.Size(); ++i)
+         {
+            if (weights[i] != 0.0)
+            {
+               B.A.Add(weights[i], Js[i]->A);
+            }
+         }
+         prec[key] = new DGPreconditioner(gamma, M, dt, B, dg.BlockSize());
+      }
       current_prec = prec[key];
+   }
+
+   ~DGIRKOperator()
+   {
+      for (DGMatrix *J : Js)
+      {
+         delete J;
+      }
    }
 };
 
@@ -208,16 +290,19 @@ double RunCase(Params &p)
    DGWrapper dg_wrapper;
    dg_wrapper.Init(p.problem);
 
-   DGIRKOperator dg_irk(dg_wrapper);
+   // DGIRKOperator dg_irk(dg_wrapper, IRKOperator::APPROXIMATE);
+   DGIRKOperator dg_irk(dg_wrapper, IRKOperator::EXACT);
    FE_Evolution evol(dg_wrapper);
 
    std::unique_ptr<ODESolver> ode;
+   std::unique_ptr<RKData> tableau;
 
    if (use_irk && !compute_exact)
    {
-      RKData::Type irk_type = RKData::RadauIIA7;
-      // RKData::Type irk_type = RKData::LSDIRK3;
-      IRK *irk = new IRK(&dg_irk, irk_type);
+      // RKData::Type irk_type = RKData::RadauIIA7;
+      RKData::Type irk_type = RKData::Gauss4;
+      tableau.reset(new RKData(irk_type));
+      IRK *irk = new IRK(&dg_irk, *tableau);
 
       IRK::KrylovParams krylov_params;
       krylov_params.reltol = 1e-5;
@@ -227,6 +312,7 @@ double RunCase(Params &p)
       IRK::NewtonParams newton_params;
       newton_params.reltol = 1e-9;
       newton_params.abstol = 1e-9;
+      newton_params.jac_update_rate = 1;
 
       irk->SetKrylovParams(krylov_params);
       irk->SetNewtonParams(newton_params);
@@ -276,9 +362,9 @@ int main(int argc, char **argv)
    MPI_Init(&argc, &argv);
 
    Params p;
-   p.tf = 1.0;
    // p.dt = 0.16;
    p.dt = 1e-3;
+   p.tf = p.dt;
    p.compute_exact = false;
    p.problem = 1;
 
@@ -293,7 +379,7 @@ int main(int argc, char **argv)
    {
       double irk_er=0.0, dirk_er=0.0;
       p.use_irk = true;
-      //irk_er = RunCase(p);
+      irk_er = RunCase(p);
       p.use_irk = false;
       dirk_er = RunCase(p);
       f << p.dt << "    " << irk_er << "    " << dirk_er << std::endl;
