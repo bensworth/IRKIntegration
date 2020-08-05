9486eeac41c2521a90d86394f9bf49aa7b1e0fbf
ben-s-southworth <ben.s.southworth@gmail.com>
2020-08-04 18:02:41 -0600

    DG advection-diffusion section mostly done

--- ODEs_linear/figures/dg_adv_diffresults/optimal/o2_k1e-2_plots.py
+++ ODEs_linear/figures/dg_adv_diffresults/optimal/o2_k1e-2_plots.py
@@ -33,6 +33,9 @@ AMG_iters = [1,2,3,4,5,6,7,8]
 gmres = [1011, 2*190, 3*39, 4*24, 5*19, 6*17, 7*14, 8*12]
 nogmres = [2000, 2*507, 3*88, 4*37, 5*26, 6*23, 7*19, 8*17]
 
+gmres1e6 = [66, 2*29, 3*28, 4*27, 5*27, 6*26, 7*26, 8*12]
+nogmres1e6 = [66, 2*29, 3*28, 4*27, 5*27, 6*26, 7*26, 8*26]
+
 fig1, axL = plt.subplots(figsize=(11,8))
 line1, = plt.plot(AMG_iters, nogmres, '-o', lw=lw, markersize=12, label='Inner fixed-point')
 line2, = plt.plot(AMG_iters, gmres, '-*', lw=lw, markersize=16, label='Inner GMRES')
@@ -51,5 +54,26 @@ plt.grid(True)
 axL.legend(handles=[line1,line2], ncol=1, frameon=True, loc='upper right')
 plt.savefig('dg_advdiff_o2_1e-2.pdf', bbox_inches='tight', transparent=True)
 plt.show()
+
+
+fig1, axL = plt.subplots(figsize=(11,8))
+line1, = plt.plot(AMG_iters, nogmres1e6, '-o', lw=lw, markersize=12, label='Inner fixed-point')
+line2, = plt.plot(AMG_iters, gmres1e6, '-*', lw=lw, markersize=16, label='Inner GMRES')
+
+axL.patch.set_visible(False)
+axL.set_xlabel("AIR iterations/FGMRES iteration", va='bottom')
+axL.yaxis.tick_left()
+axL.set_ylabel("Total AIR iterations/time step")
+axL.set_ylim((0,500))
+axL.xaxis.labelpad = 28
+axL.xaxis.grid(True)
+axL.yaxis.grid(True)
+
+# plt.title("4th-order elements")
+plt.grid(True)
+# axL.legend(handles=[line1,line2], ncol=1, frameon=True, loc='upper right')
+plt.savefig('dg_advdiff_o2_1e-6.pdf', bbox_inches='tight', transparent=True)
+plt.show()
+
 
 
