17ac49ada9dea0883b6e03632e9e323d6b0ddf8c
Ben Southworth <ben.s.southworth@gmail.com>
2021-04-27 10:23:05 -0700

    New results w H1 norm

--- IRK_lin_class/results/runtest_R.jl
+++ IRK_lin_class/results/runtest_R.jl
@@ -1,8 +1,8 @@
 methods = [
     # -3, -4,
     # 3, 4,
-    # 23, 25, 27
-    25
+    23, 25, 27
+    # 25
     # 32, 34, 36, 38
 ]
 
@@ -25,18 +25,20 @@ function mname(m)
 end
 
 for m in methods
-    # for dt in [0.1, 0.05, 0.025, 0.0125, 0.00625]
-    for dt in [0.025, 0.0125, 0.00625]
-        cmd = `srun -n72 ./ord-red_dg_adv_diff -i $m -air 1 -rs 4 -rp 2 -o 4 -dt $dt -tf 2 -e 1e-4`
+    for dt in [0.1, 0.05, 0.025, 0.0125, 0.00625]
+    # for dt in [0.025, 0.0125, 0.00625]
+        cmd = `srun -n72 ./ord-red_dg_adv_diff -i $m -air 1 -rs 4 -rp 1 -o 4 -dt $dt -tf 2 -e 1e-4`
         # cmd = `srun -n72 ./ord-red_dg_adv_diff -i $m -air 1 -rs 4 -rp 0 -o 4 -dt $dt -tf 2 -e 1e-4`
         res = read(cmd, String)
         l2 = match(r"(?<=l2 )(.*?)(?=\n)", res)
+        h1 = match(r"(?<=h1 )(.*?)(?=\n)", res)
         runtime = match(r"(?<=runtime )(.*?)(?=\n)", res)
         print(mname(m))
         println()
         println("dt ", dt)
         println("time ", runtime.match)
         println("l2 ", l2.match)
+        println("h1 ", h1.match)
         println()
         flush(stdout)
     end
