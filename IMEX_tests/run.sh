442c24d7ef2d5a1e06a5530feab16fd5902c5819
bensworth <ben.s.southworth@gmail.com>
2021-08-30 10:19:57 -0600

    rename file

--- IMEX_tests/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-srun -n120 ./test-imex-dg-adv-diff -rs 4 -rp 1 -o 4 -e 0.01 -tf 2 >> imex_tests_o4_e0.01_tf2_newbdf.txt
-wait
-srun -n120 ./test-imex-dg-adv-diff -rs 4 -rp 1 -o 4 -e 0.1 -tf 2 >> imex_tests_o4_e0.1_tf2_newbdf.txt
-wait
-srun -n120 ./test-imex-dg-adv-diff -rs 4 -rp 1 -o 4 -e 1 -tf 2 >> imex_tests_o4_e1_tf2_newbdf.txt
-wait
-srun -n120 ./test-imex-dg-adv-diff -rs 4 -rp 1 -o 4 -e 10 -tf 2 >> imex_tests_o4_e10_tf2_newbdf.txt
